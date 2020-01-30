bc3e369583519397ebcf8ff9536e7ee4eff9b71c
志宇 <evanlinjin@pm.me>
2020-01-30 23:45:38 +0800

    Added log messages.

--- termshare.go
+++ termshare.go
@@ -158,9 +158,9 @@ type bufferWriter struct {
 }
 
 func (bw *bufferWriter) Write(p []byte) (n int, err error) {
-	defer func() {
-		log.Printf("bufferWriter.Write() n(%d) err(%v) %s", n, err, p[:n])
-	}()
+	//defer func() {
+	//	log.Printf("bufferWriter.Write() n(%d) err(%v) %s", n, err, p[:n])
+	//}()
 
 	if bw.b == nil {
 		bw.b = new(bytes.Buffer)
@@ -179,7 +179,6 @@ func (bw *bufferWriter) Write(p []byte) (n int, err error) {
 			bw.w = nil
 			return bw.b.Write(p)
 		}
-		n = len(p)
 		return n, err
 	} else {
 		return bw.b.Write(p)
@@ -276,7 +275,7 @@ func createSession() {
 		eof <- true
 	}()
 	go func() {
-		io.Copy(ptyF, conn)
+		Copy(ptyF, conn)
 		eof <- true
 	}()
 	go func() {
@@ -288,6 +287,75 @@ func createSession() {
 		}
 	}()
 	<-eof
+}
+
+// Copy copies from src to dst until either EOF is reached
+// on src or an error occurs. It returns the number of bytes
+// copied and the first error encountered while copying, if any.
+//
+// A successful Copy returns err == nil, not err == EOF.
+// Because Copy is defined to read from src until EOF, it does
+// not treat an EOF from Read as an error to be reported.
+//
+// If src implements the WriterTo interface,
+// the copy is implemented by calling src.WriteTo(dst).
+// Otherwise, if dst implements the ReaderFrom interface,
+// the copy is implemented by calling dst.ReadFrom(src).
+func Copy(dst io.Writer, src io.Reader) (written int64, err error) {
+	return copyBuffer(dst, src, nil)
+}
+
+// copyBuffer is the actual implementation of Copy and CopyBuffer.
+// if buf is nil, one is allocated.
+func copyBuffer(dst io.Writer, src io.Reader, buf []byte) (written int64, err error) {
+	log.Printf("copyBuffer: start...")
+	// If the reader has a WriteTo method, use it to do the copy.
+	// Avoids an allocation and a copy.
+	if wt, ok := src.(io.WriterTo); ok {
+		return wt.WriteTo(dst)
+	}
+	// Similarly, if the writer has a ReadFrom method, use it to do the copy.
+	if rt, ok := dst.(io.ReaderFrom); ok {
+		return rt.ReadFrom(src)
+	}
+	if buf == nil {
+		size := 10// 32 * 1024
+		if l, ok := src.(*io.LimitedReader); ok && int64(size) > l.N {
+			if l.N < 1 {
+				size = 1
+			} else {
+				size = int(l.N)
+			}
+		}
+		buf = make([]byte, size)
+	}
+	for {
+		log.Printf("copyBuffer: start read...")
+		nr, er := src.Read(buf)
+		log.Printf("copyBuffer(src.Read) n(%d) err(%v) %s", nr, er, buf[0:nr])
+		if nr > 0 {
+			nw, ew := dst.Write(buf[0:nr])
+			log.Printf("copyBuffer(dst.Write) n(%d) err(%v) %s", nw, ew, buf[0:nw])
+			if nw > 0 {
+				written += int64(nw)
+			}
+			if ew != nil {
+				err = ew
+				break
+			}
+			if nr != nw {
+				err = io.ErrShortWrite
+				break
+			}
+		}
+		if er != nil {
+			if er != io.EOF {
+				err = er
+			}
+			break
+		}
+	}
+	return written, err
 }
 
 func joinSession(sessionUrl string) {
@@ -353,10 +421,10 @@ func startDaemon() {
 		default:
 			parts := strings.Split(r.RequestURI, "/")
 			sessionName := parts[1]
-			session, err := sessions.Get(sessionName)
+
 			if r.Method == "POST" {
 				r.ParseForm()
-				_, err = sessions.Create(sessionName, r.Form.Get("copilot") != "", r.Form.Get("private") != "")
+				_, err := sessions.Create(sessionName, r.Form.Get("copilot") != "", r.Form.Get("private") != "")
 				if err != nil {
 					log.Println(err)
 					w.WriteHeader(http.StatusConflict)
@@ -376,6 +444,8 @@ func startDaemon() {
 				w.Write([]byte(strings.Replace(banner, "{{URL}}", baseUrl+"/"+sessionName, 1)))
 				return
 			}
+
+			session, err := sessions.Get(sessionName)
 			if err != nil {
 				w.WriteHeader(http.StatusNotFound)
 				return
